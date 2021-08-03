583d53032126f787821b845a6958e1fa91389f6f
badsector2010 <84140666+badsector2010@users.noreply.github.com>
2021-08-03 18:45:28 +0700

    Update __init__.py

--- blogproject/__init__.py
+++ blogproject/__init__.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('MY_SECRET_KEY')
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL?sslmode=require")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
 
 from blogproject import routes
